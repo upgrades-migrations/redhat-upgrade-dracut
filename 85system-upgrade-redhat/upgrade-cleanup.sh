23cce8fec3d921cd007d1978a53af92010007721
David Shea <dshea@redhat.com>
2013-12-10 11:59:40 -0500

    Remove beefy miracle from the log
    Mine eye runneth down with rivers of water for the destruction of the
    daughter of my people.
    
    Resolves: rhbz#1031956

--- 85system-upgrade-redhat/upgrade-cleanup.sh
+++ 85system-upgrade-redhat/upgrade-cleanup.sh
@@ -2,23 +2,3 @@
 getargbool 0 rd.upgrade.test && return # skip cleanup if we were just testing
 [ "$UPGRADE_STATE" == failed ] && return # skip cleanup on failure
 chroot $NEWROOT redhat-upgrade-tool-cli --clean
-cat << '__BEEFYMIRACLE__'
-|               .---. __
-|    ,         /     \   \    ||||
-|   \\\\      |O___O |    | \\||||
-|   \   //    | \_/  |    |  \   /
-|    '--/----/|     /     |   |-'
-|           // //  /     -----'
-|          //  \\ /      /
-|         //  // /      /
-|        //  \\ /      /
-|       //  // /      /
-|      /|   ' /      /
-|      //\___/      /
-|     //   ||\     /
-|     \\_  || '---'
-|     /' /  \\_.-
-|    /  /    --| |
-|    '-'      |  |
-|              '-'
-__BEEFYMIRACLE__
