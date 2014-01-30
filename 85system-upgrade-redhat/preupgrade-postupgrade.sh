67bbcc4a0d721546b5ce6fd3cca0d94089d0b8b0
David Shea <dshea@redhat.com>
2014-01-30 17:50:26 -0500

    Sort the postupgrade scripts
    Resolves: rhbz#1054262

--- 85system-upgrade-redhat/preupgrade-postupgrade.sh
+++ 85system-upgrade-redhat/preupgrade-postupgrade.sh
@@ -6,7 +6,9 @@ echo "Running postupgrade scripts..."
 
 if [ -d "${NEWROOT}/${POSTUPGRADE_DIR}" ]; then
     ( cd $NEWROOT ;
-      for script in "./${POSTUPGRADE_DIR}"/*/* ; do
+
+      find "./${POSTUPGRADE_DIR}" -type f -perm /u+x -exec ls -1 {} + | \
+        while read -r script ; do
           echo "Running ${script##./}..."
           chroot . "$script"
        done
