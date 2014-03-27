818e7cf9c9c0c858c55a55fe903f6327c8b995eb
David Shea <dshea@redhat.com>
2014-03-27 10:21:42 -0400

    Change to the postupgrade script directory before running.
    Resolves: rhbz#1078248

--- 85system-upgrade-redhat/preupgrade-postupgrade.sh
+++ 85system-upgrade-redhat/preupgrade-postupgrade.sh
@@ -10,7 +10,9 @@ if [ -d "${NEWROOT}/${POSTUPGRADE_DIR}" ]; then
       find "./${POSTUPGRADE_DIR}" -type f -perm /u+x -exec ls -1 {} + | \
         while read -r script ; do
           echo "Running ${script##./}..."
-          chroot . "$script"
+          scriptdir="$(echo "${script}" | sed 's|^\(.*\)/\([^/]*\)$|\1|')"
+          scriptbase="$(echo "${script}" | sed 's|^\(.*\)/\([^/]*\)$|\2|')"
+          chroot . /bin/sh -c "cd \"${scriptdir}\" && ./\"${scriptbase}\""
        done
     )
 else
