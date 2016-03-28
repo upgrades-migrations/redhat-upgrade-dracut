8a5dc90f4e2e8119f3cb4dceff8f7a2e71877f63
David Shea <dshea@redhat.com>
2016-03-28 10:54:23 -0400

    Use --clean-upgraded instead of --clean
    Use a new clean argument to differentiate between clean operations run
    after a successful upgrade and clean operations initiated by the user
    before an upgrade. This allows the post-upgrade cleanup to leave certain
    files from the upgrade behind, in particular network repo files.
    
    Related: rhbz#1275622

--- 85system-upgrade-redhat/upgrade-cleanup.sh
+++ 85system-upgrade-redhat/upgrade-cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 getargbool 0 rd.upgrade.test && return # skip cleanup if we were just testing
 [ "$UPGRADE_STATE" == failed ] && return # skip cleanup on failure
-chroot $NEWROOT redhat-upgrade-tool-cli --clean
+chroot $NEWROOT redhat-upgrade-tool-cli --clean-upgraded
